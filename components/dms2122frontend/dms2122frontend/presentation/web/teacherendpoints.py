50890416b2b58e03b6ae65df6d7e6744c63a4813
Ismael <ifh1001@alu.ubu.es>
2021-11-13 21:00:29 +0100

    Edit and New Questions (mock)

--- components/dms2122frontend/dms2122frontend/presentation/web/teacherendpoints.py
+++ components/dms2122frontend/dms2122frontend/presentation/web/teacherendpoints.py
@@ -2,7 +2,7 @@
 """
 
 from typing import Text, Union
-from flask import redirect, url_for, session, render_template
+from flask import redirect, url_for, session, render_template, request
 from werkzeug.wrappers import Response
 from dms2122common.data import Role
 from dms2122frontend.data.rest.authservice import AuthService
@@ -83,7 +83,9 @@ class TeacherEndpoints():
         if Role.Teacher.name not in session['roles']:
             return redirect(url_for('get_home'))
         name = session['user']
-        return render_template('teacher/questions/new.html', name=name, roles=session['roles'])
+        redirect_to = request.args.get('redirect_to', default='/teacher/questions')
+        return render_template('teacher/questions/new.html', name=name, roles=session['roles'],
+                               redirect_to=redirect_to)
     
     @staticmethod
     def get_teacher_questions_edit(auth_service: AuthService) -> Union[Response, Text]:
@@ -100,7 +102,10 @@ class TeacherEndpoints():
         if Role.Teacher.name not in session['roles']:
             return redirect(url_for('get_home'))
         name = session['user']
-        return render_template('teacher/questions/edit.html', name=name, roles=session['roles'])
+        redirect_to = request.args.get('redirect_to', default='/teacher/questions')
+        return render_template('teacher/questions/edit.html', name=name, roles=session['roles'], 
+            redirect_to=redirect_to, title="Pregunta 1", body="Cuerpo", option1="A", option2="B",
+            option3="C",correct_answer=3)
     
     @staticmethod
     def get_teacher_questions_preview(auth_service: AuthService) -> Union[Response, Text]:
