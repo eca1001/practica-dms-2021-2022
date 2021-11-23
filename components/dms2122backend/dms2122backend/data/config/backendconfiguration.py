23c7a63093c2b514fe0176081db8ffe3bd3bbb73
dgm1003 <dgm1003@alu.ubu.es>
2021-11-23 20:52:01 +0100

    Completed BackendConfiguration

--- components/dms2122backend/dms2122backend/data/config/backendconfiguration.py
+++ components/dms2122backend/dms2122backend/data/config/backendconfiguration.py
@@ -9,8 +9,48 @@ class BackendConfiguration(ServiceConfiguration):
     """ Class responsible of storing the backend service configuration.
     """
 
-    #TODO: implement everything else
+    def _component_name(self) -> str:
+        """ The component name, to categorize the default config path.
 
+        Returns:
+            - str: A string identifying the component which will categorize the configuration.
+        """
+
+        return 'dms2122backend'
+
+    def __init__(self):
+        """ Initialization/constructor method.
+        """
+
+        ServiceConfiguration.__init__(self)
+
+        self.set_db_connection_string('sqlite:////tmp/dms2122backend.sqlite3.db')
+        self.set_service_host('127.0.0.1')
+        self.set_service_port(5000)
+        self.set_debug_flag(True)
+        self.set_password_salt('This salt should be changed ASAP')
+        self.set_authorized_api_keys([])
+        self.set_auth_service({
+            'host': '127.0.0.1',
+            'port': 4000,
+            'apikey_secret': 'This should be the backend API key'
+        })
+
+
+    def _set_values(self, values: Dict) -> None:
+        """Sets/merges a collection of configuration values.
+
+        Args:
+            - values (Dict): A dictionary of configuration values.
+        """
+        ServiceConfiguration._set_values(self, values)
+
+        if 'db_connection_string' in values:
+            self.set_db_connection_string(values['db_connection_string'])
+        if 'salt' in values:
+            self.set_password_salt(values['salt'])
+        if 'auth_service' in values:
+            self.set_auth_service(values['auth_service'])
 
     def set_db_connection_string(self, db_connection_string: str) -> None:
         """ Sets the db_connection_string configuration value.
@@ -32,4 +72,43 @@ class BackendConfiguration(ServiceConfiguration):
 
         return str(self._values['db_connection_string'])
 
-    #TODO: implement everything else
+    def set_password_salt(self, salt: str) -> None:
+        """ Sets the password salt configuration value.
+
+        Args:
+            - salt: A string with the configuration value.
+
+        Raises:
+            - ValueError: If validation is not passed.
+        """
+        self._values['salt'] = str(salt)
+
+    def get_password_salt(self) -> str:
+        """ Gets the password salt configuration value.
+
+        Returns:
+            - str: A string with the value of salt.
+        """
+
+        return str(self._values['salt'])
+
+    def set_auth_service(self, auth_service: Dict) -> None:
+        """Sets the connection parameters for the authentication service.
+
+        Args:
+            auth_service (Dict): Parameters to connect to the authentication service.
+
+        Raises:
+            - ValueError: If validation is not passed.
+        """
+        self._values['auth_service'] = auth_service
+
+    def get_auth_service(self) -> Dict:
+        """ Gets the authentication service configuration value.
+
+        Returns:
+            - Dict: A dictionary with the value of auth_service.
+        """
+
+        return self._values['auth_service']
+
