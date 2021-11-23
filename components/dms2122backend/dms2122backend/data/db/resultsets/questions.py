34cc64e3b2a4389321fc6f547c5d05ae29ee988a
Ismael <ifh1001@alu.ubu.es>
2021-11-23 21:11:44 +0100

    Questions on Resulsets

--- components/dms2122backend/dms2122backend/data/db/resultsets/questions.py
+++ components/dms2122backend/dms2122backend/data/db/resultsets/questions.py
@@ -14,7 +14,7 @@ class Questions():
     """ Class responsible of table-level questions operations.
     """
     @staticmethod
-    def create(session: Session) -> Question:
+    def create(session: Session, title: str,  body: str, option1: str, option2: str, option3: str, correct_answer: int, punctuation: float, penalty: float) -> Question:
         """ Creates a new question record.
 
         Note:
@@ -25,10 +25,57 @@ class Questions():
             - 
 
         Raises:
-            - ValueError: If either the title or the [AAAAAAAAAAAAAAAAAAAAAAA COMPLETAAAAAAAAAAAAAAAAR] is empty.
+            - ValueError: If any field is empty.
             - QuestionExistsError: If a question with the same title already exists.
 
         Returns:
             - Question: The created `Question` result.
         """
+        if not title or not body or not option1 or not option2 or not option3 or not correct_answer or not punctuation or not penalty:
+            raise ValueError('All fields are required.')
+        try:
+            new_question = Question(title, body, option1, option2, option3, correct_answer, punctuation, penalty)
+            session.add(new_question)
+            session.commit()
+            return new_question
+        except IntegrityError as ex:
+            raise QuestionExistsError(
+                'A question with title ' + title + ' already exists.'
+                ) from ex
         
+    @staticmethod
+    def list_all(session: Session) -> List[Question]:
+        """Lists every question.
+
+        Args:
+            - session (Session): The session object.
+
+        Returns:
+            - List[Question]: A list of `Question` registers.
+        """
+        query = session.query(Question)
+        return query.all()
+
+    @staticmethod
+    def get_question(session: Session, title: str,  body: str, option1: str, option2: str, option3: str, correct_answer: int, punctuation: float, penalty: float) -> Question:
+        """ Determines whether a question exists or not.
+
+        Args:
+            - title: (str): A string with the question title.
+            - body (str): A string with the question body.
+            - option1 (str): A string with option1.
+            - option2 (str): A string with option2.
+            - option3 (str): A string with option3.
+            - correct_answer (int): A integer for the correct option on question.
+            - punctuation (float): A float with the punctuation of the question.
+            - penalty (float): A float with the penalty of fail the question.
+
+        Returns:
+            - bool: `True` if a question with the given credentials exists; `False` otherwise.
+        """
+        try:
+            query = session.query(Question).filter_by(title=title, body=body, option1=option1, option2=option2, option3=option3, correct_answer=correct_answer, punctuation=punctuation, penalty=penalty)
+            query.one()
+        except NoResultFound:
+            return None
+        return query.one_or_none()
