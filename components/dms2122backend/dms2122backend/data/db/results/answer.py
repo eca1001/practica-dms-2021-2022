f7ac36c3a5f13b8c4fd428a9f7018bbc0663b632
eca1001 <eca1001@alu.ubu.es>
2021-11-23 20:46:31 +0100

    Answer Class

--- components/dms2122backend/dms2122backend/data/db/results/answer.py
+++ components/dms2122backend/dms2122backend/data/db/results/answer.py
@@ -2,22 +2,28 @@
 """
 
 from typing import Dict
-from sqlalchemy import Table, MetaData, Column, String  # type: ignore
+from sqlalchemy import Table, MetaData, ForeignKey, Column, String  # type: ignore
 from sqlalchemy.orm import relationship  # type: ignore
 from dms2122backend.data.db.results.resultbase import ResultBase
+from dms2122auth.data.db.results.user import User
 
 class Answer(ResultBase):
     """ Definition and storage of answer ORM records.
     """
 
-    def __init__(self):
+    def __init__(self, user: str, number: int, title: str):
         """ Constructor method.
 
         Initializes an answer record.
 
         Args:
-            - 
+            User: A string with students' name
+            Number: Student questions' answer
+            Tile: Questions' title
         """
+        self.user : str = user
+        self.number : int = number
+        self.title : str = title
 
     @staticmethod
     def _table_definition(metadata: MetaData) -> Table:
@@ -30,12 +36,10 @@ class Answer(ResultBase):
         Returns:
             - Table: A `Table` object with the table definition.
         """
-
-    @staticmethod
-    def _mapping_properties() -> Dict:
-        """ Gets the mapping properties dictionary.
-
-        Returns:
-            - Dict: A dictionary with the mapping properties.
-        """
-        
+        return Table(
+            'answers',
+            metadata,
+            Column('user', String(32), ForeignKey('user.username'), primary_key=True),
+            Column('number', int, primary_key=True),
+            Column('title', String(64), nullable=False)
+        )
