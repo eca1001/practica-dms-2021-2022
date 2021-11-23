bfbe21062986522726457bb34d5d9b15f9fb34cc
Ismael <ifh1001@alu.ubu.es>
2021-11-23 20:34:33 +0100

    Question Class

--- components/dms2122backend/dms2122backend/data/db/results/question.py
+++ components/dms2122backend/dms2122backend/data/db/results/question.py
@@ -2,7 +2,7 @@
 """
 
 from typing import Dict
-from sqlalchemy import Table, MetaData, Column, String  # type: ignore
+from sqlalchemy import Table, MetaData, Column, String, Integer, Float  # type: ignore
 from sqlalchemy.orm import relationship  # type: ignore
 from dms2122backend.data.db.results.resultbase import ResultBase
 
@@ -10,14 +10,29 @@ class Question(ResultBase):
     """ Definition and storage of question ORM records.
     """
 
-    def __init__(self):
+    def __init__(self, title: str,  body: str, option1: str, option2: str, option3: str, correct_answer: int, punctuation: float, penalty: float):
         """ Constructor method.
 
         Initializes a question record.
 
         Args:
-            - 
+            - title: (str): A string with the question title.
+            - body (str): A string with the question body.
+            - option1 (str): A string with option1.
+            - option2 (str): A string with option2.
+            - option3 (str): A string with option3.
+            - correct_answer (int): A integer for the correct option on question.
+            - punctuation (float): A float with the punctuation of the question.
+            - penalty (float): A float with the penalty of fail the question.
         """
+        self.title: str = title
+        self.body: str = body
+        self.option1: str = option1
+        self.option2: str = option2
+        self.option3: str = option3
+        self.correct_answer: int = correct_answer
+        self.punctuation: float = punctuation
+        self.penalty: float = penalty
 
     @staticmethod
     def _table_definition(metadata: MetaData) -> Table:
@@ -31,11 +46,16 @@ class Question(ResultBase):
             - Table: A `Table` object with the table definition.
         """
 
-    @staticmethod
-    def _mapping_properties() -> Dict:
-        """ Gets the mapping properties dictionary.
-
-        Returns:
-            - Dict: A dictionary with the mapping properties.
-        """
-        
+        return Table(
+            'questions',
+            metadata,
+            Column('title', String(64), primary_key=True),
+            Column('body', String(256), nullable=False),
+            Column('option1', String(256), nullable=False),
+            Column('option2', String(256), nullable=False),
+            Column('option3', String(256), nullable=False),
+            Column('correct_answer', Integer, nullable=False),
+            Column('punctuation', Float(2,2), nullable=False),
+            Column('penalty', Float(2,2), nullable=False)
+        )
+         
