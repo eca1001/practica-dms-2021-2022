47ba2ef1bad36af25a04214f660bfe0d4f825488
eca1001 <eca1001@alu.ubu.es>
2021-12-03 13:28:41 +0100

    Fix Answer 2

--- components/dms2122backend/dms2122backend/presentation/rest/answer.py
+++ components/dms2122backend/dms2122backend/presentation/rest/answer.py
@@ -3,13 +3,14 @@
 
 from typing import Tuple, Union, Optional, List, Dict
 from http import HTTPStatus
-from flask import current_app
+from flask import current_app, session
 from dms2122backend.data.db.exc import QuestionExistsError
 from dms2122backend.service import AnswersServices
-from dms2122auth.service.roleservices import  RoleServices
+from dms2122backend.data.rest.authservice import AuthService
 from dms2122common.data.role import Role
+from dms2122common.data.rest import ResponseData
 
-def answer(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], Optional[int]]:
+def answer(authservice: AuthService, body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], Optional[int]]:
     """Answer a question if the requestor has the Student role.
 
     Args:
@@ -24,21 +25,22 @@ def answer(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], Optional[int
             - 409 CONFLICT if an existing user already has all or part of the unique user's data.
     """
     with current_app.app_context():
-        if not RoleServices.has_role(token_info['user_token']['user'], Role.Student, current_app.db):
+        response: ResponseData = authservice.get_user_has_role(session.get('token'), token_info['user_token']['user'], "Student")
+        if response.is_successful() == False:
             return (
                 'Current user has not enough privileges to create a question',
                 HTTPStatus.FORBIDDEN.value
             )
         try:
-            answer: Dict = AnswersServices.answer(
+            AnswersServices.answer(
                 body['session'], body['username'],  body['number'], body['questionId']
             )
         except ValueError:
             return ('A mandatory argument is missing', HTTPStatus.BAD_REQUEST.value)
-    return (answer, HTTPStatus.OK.value)
+    return (None, HTTPStatus.OK.value)
 
 
-def list_all_for_user(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], Optional[int]]:
+def list_all_for_user(authservice: AuthService, body: Dict, token_info: Dict) -> Tuple[Union[List[str], str], Optional[int]]:
     """List all question of an specific user if the requestor has the Student role.
 
     Args:
@@ -53,13 +55,14 @@ def list_all_for_user(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], O
             - 409 CONFLICT if an existing user already has all or part of the unique user's data.
     """
     with current_app.app_context():
-        if not RoleServices.has_role(token_info['user_token']['user'], Role.Student, current_app.db):
+        response: ResponseData = authservice.get_user_has_role(session.get('token'), token_info['user_token']['user'], "Student")
+        if response.is_successful() == False:
             return (
                 'Current user has not enough privileges to create a question',
                 HTTPStatus.FORBIDDEN.value
             )
         try:
-            answer = AnswersServices.list_all_for_user(
+            answer: List[str] = AnswersServices.list_all_for_user(
                 body['user'], current_app.db
             )
         except ValueError:
@@ -67,7 +70,7 @@ def list_all_for_user(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], O
     return (answer, HTTPStatus.OK.value)
 
 
-def list_all_for_question(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], Optional[int]]:
+def list_all_for_question(authservice: AuthService, body: Dict, token_info: Dict) -> Tuple[Union[List[str], str], Optional[int]]:
     """List all answers of an specific question if the requestor has the Teacher role.
 
     Args:
@@ -82,20 +85,21 @@ def list_all_for_question(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str
             - 409 CONFLICT if an existing user already has all or part of the unique user's data.
     """
     with current_app.app_context():
-        if not RoleServices.has_role(token_info['user_token']['user'], Role.Teacher, current_app.db):
+        response: ResponseData = authservice.get_user_has_role(session.get('token'), token_info['user_token']['user'], "Teacher")
+        if response.is_successful() == False:
             return (
                 'Current user has not enough privileges to create a question',
                 HTTPStatus.FORBIDDEN.value
             )
         try:
-            answer = AnswersServices.list_all_for_question(
+            answer: List[str] = AnswersServices.list_all_for_question(
                 body['id'], current_app.db
             )
         except ValueError:
             return ('A mandatory argument is missing', HTTPStatus.BAD_REQUEST.value)        
     return (answer, HTTPStatus.OK.value)
 
-def question_has_answers(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str], Optional[int]]:
+def question_has_answers(authservice: AuthService, body: Dict, token_info: Dict) -> Tuple[bool, Optional[int]]:
     """List all answers of an specific question if the requestor has the Teacher role.
 
     Args:
@@ -110,7 +114,8 @@ def question_has_answers(body: Dict, token_info: Dict) -> Tuple[Union[Dict, str]
             - 409 CONFLICT if an existing user already has all or part of the unique user's data.
     """
     with current_app.app_context():
-        if not RoleServices.has_role(token_info['user_token']['user'], Role.Teacher, current_app.db):
+        response: ResponseData = authservice.get_user_has_role(session.get('token'), token_info['user_token']['user'], "Teacher")
+        if response.is_successful() == False:
             return (
                 'Current user has not enough privileges to create a question',
                 HTTPStatus.FORBIDDEN.value
