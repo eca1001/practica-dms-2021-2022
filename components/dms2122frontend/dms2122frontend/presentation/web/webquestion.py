ee54c840886316421021ab55a5f637bc2f30b823
dgm1003 <dgm1003@alu.ubu.es>
2021-12-05 20:20:52 +0100

    finished webquestion

--- components/dms2122frontend/dms2122frontend/presentation/web/webquestion.py
+++ components/dms2122frontend/dms2122frontend/presentation/web/webquestion.py
@@ -45,6 +45,23 @@ class WebQuestion():
         return response.get_content()
 
     @staticmethod
+    def edit_question(backend_service: BackendService, id: int, title: str,  body: str, option1: str, option2: str, 
+            option3: str, correct_answer: int, punctuation: float, penalty: float) -> Optional[Dict]:
+        """ Edits a question in the backend service.
+
+        Args:
+            - backend_service (BackendService): The backend service.
+
+        Returns:
+            - Dict: A dictionary with the edited question if successful.
+            - None: Nothing on error.
+        """
+        response: ResponseData = backend_service.edit_question(session.get('token'), id, title, body, option1, option2,
+                        option3, correct_answer, punctuation, penalty)
+        WebUtils.flash_response_messages(response)
+        return response.get_content()
+
+    @staticmethod
     def get_question(backend_service: BackendService, id: int) -> Optional[Dict]:
         """ gets a question from the backend service.
 
@@ -58,3 +75,18 @@ class WebQuestion():
         response: ResponseData = backend_service.get_question(session.get('token'), id)
         WebUtils.flash_response_messages(response)
         return response.get_content()
+
+
+    @staticmethod
+    def question_has_answers(backend_service: BackendService, id: id) -> bool:
+        """ Updates the user roles in the authentication service.
+
+        Args:
+            - backend_service (BackendService): The backend service.
+
+        Returns:
+            - bool: Whether the question has answers (`True`) or not (`False`)
+        """
+        response: ResponseData = backend_service.question_has_answers(session.get('token'), id)
+        WebUtils.flash_response_messages(response)
+        return response.is_successful()
