33987770f8f144f3217e8d7e439897018a0338cc
dgm1003 <dgm1003@alu.ubu.es>
2021-12-19 01:50:23 +0100

    Added backend services to student endpoints

--- components/dms2122frontend/dms2122frontend/presentation/web/studentendpoints.py
+++ components/dms2122frontend/dms2122frontend/presentation/web/studentendpoints.py
@@ -6,6 +6,7 @@ from flask import redirect, url_for, session, render_template, request
 from werkzeug.wrappers import Response
 from dms2122common.data import Role
 from dms2122frontend.data.rest.authservice import AuthService
+from dms2122frontend.data.rest.backendservice import BackendService
 from .webauth import WebAuth
 from .webquestion import WebQuestion
 from .webanswer import WebAnswer
@@ -49,7 +50,7 @@ class StudentEndpoints():
         return render_template('student/questions.html', name=name, roles=session['roles'])
     
     @staticmethod
-    def get_student_progress(auth_service: AuthService) -> Union[Response, Text]:
+    def get_student_progress(auth_service: AuthService, backend_service: BackendService) -> Union[Response, Text]:
         """ Handles the GET requests to the student progress endpoint.
 
         Args:
@@ -66,7 +67,7 @@ class StudentEndpoints():
         return render_template('student/progress.html', name=name, roles=session['roles'])
 
     @staticmethod
-    def get_student_questions_answered(auth_service: AuthService) -> Union[Response, Text]:
+    def get_student_questions_answered(auth_service: AuthService, backend_service: BackendService) -> Union[Response, Text]:
         """ Handles the GET requests to the student's answered questions endpoint.
 
         Args:
@@ -84,10 +85,10 @@ class StudentEndpoints():
         #test values until backfront is completed
         answers=[ {"title" : "Pregunta de prueba 3", "score" : "1"}]
         return render_template('student/questions/answered.html', name=name, roles=session['roles'],
-                                answers=answers)
+                                answers=WebAnswer.list_all_for_user(backend_service, name))
 
     @staticmethod
-    def get_student_questions_answered_view(auth_service: AuthService) -> Union[Response, Text]:
+    def get_student_questions_answered_view(auth_service: AuthService, backend_service: BackendService) -> Union[Response, Text]:
         """ Handles the GET requests to the student's answered questions viewing endpoint.
 
         Args:
@@ -107,7 +108,7 @@ class StudentEndpoints():
                                 roles=session['roles'], redirect_to=redirect_to, title=title)
 
     @staticmethod
-    def get_student_questions_pending(auth_service: AuthService) -> Union[Response, Text]:
+    def get_student_questions_pending(auth_service: AuthService, backend_service: BackendService) -> Union[Response, Text]:
         """ Handles the GET requests to the student's pending questions endpoint.
 
         Args:
@@ -135,7 +136,7 @@ class StudentEndpoints():
                             questions=questions)
 
     @staticmethod
-    def get_student_questions_pending_answer(auth_service: AuthService) -> Union[Response, Text]:
+    def get_student_questions_pending_answer(auth_service: AuthService, backend_service: BackendService) -> Union[Response, Text]:
         """ Handles the GET requests to the question answering endpoint.
 
         Args:
@@ -155,7 +156,7 @@ class StudentEndpoints():
                                 roles=session['roles'], redirect_to=redirect_to, title=title)
 
     @staticmethod
-    def post_student_questions_pending_answer(auth_service: AuthService) -> Union[Response, Text]:
+    def post_student_questions_pending_answer(auth_service: AuthService, backend_service: BackendService) -> Union[Response, Text]:
         """ Handles the GET requests to the question answering endpoint.
 
         Args:
